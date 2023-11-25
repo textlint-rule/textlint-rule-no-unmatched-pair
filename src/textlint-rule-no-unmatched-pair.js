861beb3c836a7e7a170ba6690f3729ba0dd13471
azu <azuciao@gmail.com>
2023-11-25 12:27:14 +0900

    chore(deps): update to sentence-splitter@5

--- src/textlint-rule-no-unmatched-pair.js
+++ src/textlint-rule-no-unmatched-pair.js
@@ -4,7 +4,7 @@ import { PairMaker } from "./parser/PairMaker.js";
 import { SourceCode } from "./parser/SourceCode.js";
 import { IgnoreNodeManager } from "textlint-rule-helper";
 
-const report = context => {
+const report = (context) => {
     const { Syntax, report, RuleError } = context;
     const ignoreNodeManager = new IgnoreNodeManager();
     return {
@@ -19,29 +19,37 @@ const report = context => {
                 Syntax.BlockQuote,
                 Syntax.Comment
             ]);
-            sentences.children.filter(node => node.type === SentenceSplitterSyntax.Sentence).forEach(sentence => {
-                const source = new SourceCode(sentence.raw);
-                const pairMaker = new PairMaker();
-                const sentenceIndex = sentence.range[0];
-                while (source.canRead) {
-                    // If the character is in ignored range, skip it
-                    const characterIndex = sentenceIndex + source.index;
-                    // console.log(characterIndex, source.text[source.index], ignoreNodeManager.isIgnoredIndex(characterIndex));
-                    if (!ignoreNodeManager.isIgnoredIndex(characterIndex)) {
-                        pairMaker.mark(source);
+            sentences.children
+                .filter((node) => node.type === SentenceSplitterSyntax.Sentence)
+                .forEach((sentence) => {
+                    const source = new SourceCode(sentence.raw);
+                    const pairMaker = new PairMaker();
+                    const sentenceIndex = sentence.range[0];
+                    while (source.canRead) {
+                        // If the character is in ignored range, skip it
+                        const characterIndex = sentenceIndex + source.index;
+                        // console.log(characterIndex, source.text[source.index], ignoreNodeManager.isIgnoredIndex(characterIndex));
+                        if (!ignoreNodeManager.isIgnoredIndex(characterIndex)) {
+                            pairMaker.mark(source);
+                        }
+                        source.peek();
                     }
-                    source.peek();
-                }
-                // Report Error for each existing context keys
-                source.contextLocations.forEach((contextLocation) => {
-                    report(node, new RuleError(`Not found pair character for ${contextLocation.pairMark.start}.
+                    // Report Error for each existing context keys
+                    source.contextLocations.forEach((contextLocation) => {
+                        report(
+                            node,
+                            new RuleError(
+                                `Not found pair character for ${contextLocation.pairMark.start}.
                     
 You should close this sentence with ${contextLocation.pairMark.end}.
-This pair mark is called ${contextLocation.pairMark.key}.`, {
-                        index: (sentenceIndex - node.range[0]) + contextLocation.index
-                    }));
+This pair mark is called ${contextLocation.pairMark.key}.`,
+                                {
+                                    index: sentenceIndex - node.range[0] + contextLocation.index
+                                }
+                            )
+                        );
+                    });
                 });
-            });
         }
     };
 };
